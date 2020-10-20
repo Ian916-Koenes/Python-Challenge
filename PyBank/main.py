2d049d7d0a0b1086ac4319c1c35a0d7cc3f130db
Ian_Koenes <iankoenes@gmail.com>
2020-10-19 21:56:33 -0700

    added code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,28 @@ import os
 import csv
 
 #set path for file
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
-
+csvpath = os.path.join("Resources", "budget_data.csv")
+print(csvpath)
 
 with open(csvpath) as csvfile:
     
-    csvreader = csv.reader(csv, delimiter=",")
-    print(cvsreader)
+    csvreader=csv.reader(csvfile, delimiter=",")
+    print(csvpath)
     csv_header=next(csvreader)
 
+    count = 0
     for row in csvreader:
+        print(row)
+        count +=1
+
+    print(f"count={count}")
+
+    net = 0
+    for row in csvreader:
+       P_L = int(row)
+net == net + P_L
+print(f"Net Profit Loss = {net} ")
+   
+
+        
         
