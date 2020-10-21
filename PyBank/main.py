fafb6e612b0ef4818b26dd443e8736e8f8d427dd
Ian_Koenes <iankoenes@gmail.com>
2020-10-21 00:52:44 -0700

    added code

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,14 +15,31 @@ with open(csvpath) as csvfile:
     count = 0
     net = 0
     P_L = 0
+    Average = 0
+    Change = 0
+    Prior_P_L = 0
+    Sum_Change = 0
+    somethingelse = 0
+    Averge_Change = 0
+   
+    somethingelse = []
     for row in csvreader:
         print(row)
         count +=1
         P_L = int(row[1])
         net = net + P_L
-
+        Change = P_L-Prior_P_L
+        somethingelse.append(Change)
+        Sum_Change = Sum_Change + Change
+        Prior_P_L = P_L
+        #Average_Change = Sum_Change / (count-1)
+    Average_Change = sum(somethingelse[1:])/len(somethingelse[1:]) 
+    print(Average_Change)
     print(f"count={count}")
     print(f"Net Profit Loss = {net} ")
+    print(somethingelse)
+    #print(f"Average_Change =  {Average_Change} ")
+
    
 
         
