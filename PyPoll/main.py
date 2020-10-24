c4f250a9bb0088b52195aaccdf5a20a6fbc2615f
Ian_Koenes <iankoenes@gmail.com>
2020-10-23 21:25:40 -0700

    added unique list code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,9 +13,29 @@ with open(csvpath) as csvfile:
     csv_header=next(csvreader)
 
     count = 0
+    #Votes = 0
+    Candidates = 0
+    #Vote_List = []
+    #Candidate_List = []
 
     for row in csvreader:
       count += 1
+      Candidates = str(row[2])
+      #Votes = int(row[0])
+      # function to get unique values 
+      def unique(list1): 
+  
+    # intilize a null list 
+      unique_list = [] 
+      
+    # traverse for all elements 
+      for x in list1: 
+        # check if exists in unique_list or not 
+        if x not in unique_list: 
+            unique_list.append(candidates) 
+    # print list 
+    for x in unique_list: 
+        print(Candidates)
 
     print(f"count={count}")
 
